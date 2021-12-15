78fc076d3a301c0b91d4eacdd9739cb815e2d713
kamal14rajput <kamal14rajput@gmail.com>
2021-12-15 09:09:52 +0530

    UPDATED: gambler bet points

--- src/com/GamblerProgram/WinOrLoss.java
+++ src/com/GamblerProgram/WinOrLoss.java
@@ -3,17 +3,19 @@ package com.GamblerProgram;
 public class WinOrLoss {
 
 	public static void main(String[] args) {
-		int bet = 1;
+		int bet = 100;
 		int win = 1;
 		double winOrLoss = Math.floor(Math.random() * 10) % 2;
 		if (win == winOrLoss) {
 			System.out.println("you won!!!");
 			bet = bet +1;
-			System.out.println("your current bet is" + bet);
+			System.out.println("your current bet is " + bet);
 		}
 		else
 		{
 			System.out.println("you loss!!!Try your luck next time");
+			bet = bet -1;
+			System.out.println("your remaining bets are: " + bet);
 		}
 	}
 

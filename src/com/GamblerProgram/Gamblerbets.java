889ed225cb07a1a74fd2390f4cbb43c5f5d07ba3
kamal14rajput <kamal14rajput@gmail.com>
2021-12-15 23:21:10 +0530

    MODIFIED: variables declaration

--- src/com/GamblerProgram/Gamblerbets.java
+++ src/com/GamblerProgram/Gamblerbets.java
@@ -3,8 +3,8 @@ package com.GamblerProgram;
 public class Gamblerbets {
 
 	public static void main(String[] args) {
-		int totalRupees = 100;
-		int perGame = 1;
+		int stake = 100;
+		int bet = 1;
 	}
 
 }

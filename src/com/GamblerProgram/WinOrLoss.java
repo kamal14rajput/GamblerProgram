dcd21516e4d935fa92d4ccf6d53e96686d8aa5ed
kamal14rajput <kamal14rajput@gmail.com>
2021-12-15 09:20:23 +0530

    MODIFIED: variables declaration

--- src/com/GamblerProgram/WinOrLoss.java
+++ src/com/GamblerProgram/WinOrLoss.java
@@ -3,19 +3,19 @@ package com.GamblerProgram;
 public class WinOrLoss {
 
 	public static void main(String[] args) {
-		int bet = 100;
-		int win = 1;
+		int stake = 100;
+		int bet = 1;
 		double winOrLoss = Math.floor(Math.random() * 10) % 2;
-		if (win == winOrLoss) {
+		if (bet == winOrLoss) {
 			System.out.println("you won!!!");
-			bet = bet +1;
-			System.out.println("your current bet is " + bet);
+			stake = stake +1;
+			System.out.println("your current bet is " + stake);
 		}
 		else
 		{
 			System.out.println("you loss!!!Try your luck next time");
-			bet = bet -1;
-			System.out.println("your remaining bets are: " + bet);
+			stake = stake -1;
+			System.out.println("your remaining bets are: " + stake);
 		}
 	}
 
